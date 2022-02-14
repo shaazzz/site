238c55054d55b5f9749b270318ec3eb5e3531415
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2022-02-14 16:50:22 +0330

    Add inoi archive (#5)
    * Add generate table script
    
    * Add archive data
    
    * Add initial archive page
    
    * Fix last year navigation
    
    * Update archive data
    
    * Fix end newline

---
+++ scripts/generate_table.py
@@ -0,0 +1,30 @@
+import yaml
+
+def generate_head(head):
+    return f"|{'|'.join(head)}|\n|{':-:|'*len(head)}\n"
+
+def get_only_key(json):
+    for key in json:
+        return key
+    raise KeyError("empty json")
+
+def generate_cell(cell):
+    if type(cell) == str:
+        return cell
+    if cell is None:
+        return ''
+    links = []
+    for text, link in cell.items():
+        links.append(f'[{text}]({link})')
+    return '، '.join(links)
+
+def generate_row(row):
+    return f"|{'|'.join(map(generate_cell, row))}|\n"
+
+with open('archive.yml', 'r') as file:
+    data = yaml.load(file)
+
+table = generate_head(data['head'])
+for row in data["body"]:
+    table += generate_row(row)
+print(table)
